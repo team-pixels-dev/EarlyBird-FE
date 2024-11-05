45a6d1ab8ae2412ba6446adb5b96baf124d68711
jayou2323 <jayou1223@gmail.com>
2024-11-05 22:24:11 +0900

    .

--- modules/axios/client.ts
+++ modules/axios/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+import { api_url } from "@/env"; "@/env"
 
-const client = axios.create({baseURL : "https://earlybirdteam.com/"});
+const client = axios.create({baseURL : api_url});
 
 export default client;
